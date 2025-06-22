d6f3fd683703ae6c21545f849778a7c69676f5d5
Bryan Schmidt <bryan.schmidty@gmail.com>
2025-06-22 16:50:36 -0600

    FEATURE :: Add nudge functionality

--- resources/views/games/show.blade.php
+++ resources/views/games/show.blade.php
@@ -3,4 +3,10 @@
 @section('content')
 <h1>{{ $game->name }}</h1>
 <p>State: {{ $game->state }}</p>
+@if(isset($canNudge) && $canNudge)
+<form method="post" action="/games/{{ $game->game_id }}/nudge">
+    @csrf
+    <button type="submit">Nudge</button>
+</form>
+@endif
 @endsection
