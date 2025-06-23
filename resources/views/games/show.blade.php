64c254cda65c85a5dc6d8d6b39ddcbb0900da49e
Bryan Schmidt <bryan.schmidty@gmail.com>
2025-06-22 18:10:32 -0600

    Add navigation links to game view

--- resources/views/games/show.blade.php
+++ resources/views/games/show.blade.php
@@ -1,6 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
+<ul id="buttons" class="flex gap-4 mb-4">
+    <li><a href="{{ route('games.index') }}" class="text-indigo-600 hover:underline">Main Page</a></li>
+    <li><a href="{{ route('games.show', $game->game_id) }}" class="text-indigo-600 hover:underline">Reload Game Board</a></li>
+</ul>
 <h1 class="text-2xl font-semibold mb-4">{{ $game->name }}</h1>
 <p class="mb-4">State: {{ $game->state }}</p>
 @if(isset($canNudge) && $canNudge)
